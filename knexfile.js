1403e70e1c8d003f94c47ea69225343268b19980
Vitor <torquatovitor68@gmail.com>
2024-02-13 08:53:49 +0100

    building

--- knexfile.js
+++ knexfile.js
@@ -6,8 +6,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src" , "database" , "databes.db");
+      filename: path.resolve(__dirname, "src" , "database" , "database.db")
     },
+
+    migrations: {
+      directory: path.resolve(__dirname , "src" , "database" , "knex" , "migrations")
+    },
+
     useNullAsDefault: true
   }
 
