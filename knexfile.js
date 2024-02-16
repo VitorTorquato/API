7d636a8a76bed21259500d5ce9a9a40330f0f0a8
Vitor <torquatovitor68@gmail.com>
2024-02-16 14:26:49 +0100

    building

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
     },
 
     pool:{
-      afterCreate: (conn , cb ) => conn.run('PRAGMA foreing_key = ON ' , cb)
+      afterCreate: (conn , cb ) => conn.run("PRAGMA foreing_keys = ON" , cb)
     },
     migrations: {
       directory: path.resolve(__dirname , "src" , "database" , "knex" , "migrations")
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 
 const usersRouter = require("./users.routes");
+const notesRoutes = require("./notes.routes");
 
 const routes = Router();
 
 routes.use("/user" , usersRouter)
+routes.use("/notes" , notesRoutes)
 
 module.exports = routes
