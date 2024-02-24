78e83f87e2c4390cc2a67a675be1850a5fc92651
Vitor <torquatovitor68@gmail.com>
2024-02-24 09:52:54 +0100

    building

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,14 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const notesRoutes = require("./notes.routes");
+const tagsRoutes = require("./tags.routes");
+
 
 const routes = Router();
 
-routes.use("/user" , usersRouter)
-routes.use("/notes" , notesRoutes)
+routes.use("/user" , usersRouter);
+routes.use("/notes" , notesRoutes);
+routes.use("/tags" , tagsRoutes);
+
 
 module.exports = routes
