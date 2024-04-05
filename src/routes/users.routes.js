d356ce0085ad5a100032130c0a63080e2fb6488b
Vitor <torquatovitor68@gmail.com>
2024-04-05 13:11:24 +0200

    photo upload and userAvatar finished

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,20 +6,20 @@ const uploadConfig = require("../configs/upload");
 
 const UsersControllers = require("../controller/UsersControllers");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+const UserAvatarController = require("../controller/UserAvatarController");
+
 
 const usersRoutes = Router(); 
 const upload = multer(uploadConfig.MULTER);
 
 
+const userAvatarContorller = new UserAvatarController();
 const usersControllers = new UsersControllers()
 
 
 usersRoutes.post("/",usersControllers.create); 
 usersRoutes.put("/", ensureAuthenticated, usersControllers.upadte); 
-usersRoutes.patch("/avatar" , ensureAuthenticated , upload.single("avatar"), (request , respose) => {console.log(request.file.filename);
-   respose.json(); 
-
-} )
+usersRoutes.patch("/avatar" , ensureAuthenticated , upload.single("avatar") , userAvatarContorller.update )
    
 
-module.exports = usersRoutes;
+module.exports = usersRoutes;UserAvatarController
--- src/server.js
+++ src/server.js
@@ -8,11 +8,16 @@ const routes = require("./routes");
 
 const AppError = require("./utils/AppError");
 
+const uploadConfig = require("./configs/upload");
+
 
 const app = express();
 app.use(express.json());
 
+app.use("/files" , express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
+
 
 
 
